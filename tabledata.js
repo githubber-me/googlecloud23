2f991caa65184c98f6b448a5602673a992e29af4
githubber-me <kirikminchu@gmail.com>
2023-09-29 12:37:22 +0530

    Search and clear

--- tabledata.js
+++ tabledata.js
@@ -5,16 +5,24 @@ fetch("vercel.json")
 .then(function(products){
     let placeholder = document.querySelector("#data-output");
     let out = "";
+    
     for(let vercel of products){
+        let q;
+        if(vercel.gc == 1){
+            q = "Completed";
+        }
+        else{
+            q = "Not Completed";
+        }
         out += `
         <tr>
             <td>${vercel.sn}</td>
             <td>${vercel.se}</td>
-            <td>${vercel.sn}</td>
+            <td>${vercel.edt}</td>
             <td><a href="${vercel.url}">${vercel.sn}'s profile</a></td> 
             <td>${vercel.cc}</td>
             <td>${vercel.bc}</td>
-            <td>${vercel.gc}</td>
+            <td>${q}</td>
             <td>${vercel.bp}</td>
             <td>${vercel.rs}</td>
         </tr>
@@ -22,4 +30,26 @@ fetch("vercel.json")
 
     }
     placeholder.innerHTML = out;
-})
+    function filterTable() {
+        let input = document.getElementById("search-input").value.toLowerCase();
+        let rows = document.querySelectorAll("#data-output tr");
+
+        rows.forEach(function(row) {
+            let name = row.querySelector("td:first-child").textContent.toLowerCase();
+
+            if (name.includes(input)) {
+                row.style.display = "";
+            } else {
+                row.style.display = "none";
+            }
+        });
+    }
+
+    function clearSearch() {
+        document.getElementById("search-input").value = "";
+        filterTable();
+    }
+
+    document.getElementById("search-button").addEventListener("click", filterTable);
+    document.getElementById("clear-button").addEventListener("click", clearSearch);
+});
